45854b8fd2f644da8885da7099bb13b838f0b7a1
Viktor Alenkov <viktor@alenkov.name>
2023-05-07 10:38:07 +0300

    Updating dependencies

--- env-reader.go
+++ env-reader.go
@@ -1,18 +1,32 @@
 package config
 
 import (
-	"github.com/caarlos0/env/v7"
+	"github.com/caarlos0/env/v8"
 	"github.com/rs/zerolog"
 )
 
-func ReadEnvConfig(cfg any, opts ...env.Options) error {
-	return ReadEnvConfigWithLogger(zerolog.DefaultContextLogger, cfg, opts...)
+func ReadEnvConfig(cfg any, opts *env.Options) error {
+	return ReadEnvConfigWithLogger(zerolog.DefaultContextLogger, cfg, opts)
 }
 
-func ReadEnvConfigWithLogger(logger *zerolog.Logger, cfg any, opts ...env.Options) error {
+func ReadEnvConfigWithLogger(logger *zerolog.Logger, cfg any, opts *env.Options) error {
 	logger.Debug().Msgf("config input: %++v", cfg)
 
-	if err := env.Parse(cfg, opts...); err != nil {
+	logger.Trace().Msgf("options: %++v", opts)
+
+	if opts == nil {
+		if err := env.Parse(cfg); err != nil {
+			logger.Error().Err(err).Msg("Failed to read configuration from environment")
+
+			return err
+		}
+
+		logger.Trace().Msgf("cfg output: %++v", cfg)
+
+		return nil
+	}
+
+	if err := env.ParseWithOptions(cfg, *opts); err != nil {
 		logger.Error().Err(err).Msg("Failed to read configuration from environment")
 
 		return err
--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/itbasis/go-core-utils
 go 1.20
 
 require (
-	github.com/caarlos0/env/v7 v7.1.0
+	github.com/caarlos0/env/v8 v8.0.0
 	github.com/rs/zerolog v1.29.1
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 )
